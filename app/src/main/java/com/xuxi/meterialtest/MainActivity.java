b6b2008f534bfb7d02b81455add5bb1f8f6b1715
zqzal <936952020@qq.com>
2019-08-22 15:25:07 +0800

    DrawerLayout 引用

--- app/src/main/java/com/xuxi/meterialtest/MainActivity.java
+++ app/src/main/java/com/xuxi/meterialtest/MainActivity.java
@@ -2,11 +2,18 @@ package com.xuxi.meterialtest;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import android.os.Bundle;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +21,33 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.toolbar,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+
+            case R.id.backup:
+                Toast.makeText(this,"You clicked Backup",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.delete:
+                Toast.makeText(this,"You clicked Delete",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.setting:
+                Toast.makeText(this,"You clicked setting",Toast.LENGTH_SHORT).show();
+                break;
+                default:
+                    break;
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
