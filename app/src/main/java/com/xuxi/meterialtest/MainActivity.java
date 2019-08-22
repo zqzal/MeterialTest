cefcf7b70c5bf1b7504c3b130ee3b4189feec480
zqzal <936952020@qq.com>
2019-08-22 15:53:37 +0800

    问题代码提交

--- app/src/main/java/com/xuxi/meterialtest/MainActivity.java
+++ app/src/main/java/com/xuxi/meterialtest/MainActivity.java
@@ -1,28 +1,30 @@
 package com.xuxi.meterialtest;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
+import android.widget.Toolbar;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    private DrawerLayout mDrawerLayout;
+    private DrawerLayout drawerLayout;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        Toolbar toolbar = findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
         Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        drawerLayout = findViewById(R.id.drawer_layout);
 
-        mDrawerLayout = findViewById(R.id.drawer_layout);
 
     }
 
